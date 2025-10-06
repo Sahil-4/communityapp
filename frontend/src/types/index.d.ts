2319a4a18beccfe4592b6a724b639623a6e001b8
Sahil-4 <www.sahil.875020@gmail.com>
2025-10-06 14:59:26 +0530

    add: types

--- frontend/src/types/index.d.ts
+++ frontend/src/types/index.d.ts
@@ -1,1 +1,30 @@
 // global types
+
+interface User {
+  id: String;
+  username: String;
+  name: String;
+  phone: String;
+  password: String;
+  refreshToken: String | null;
+  createdAt: DateTime;
+  updatedAt: DateTime;
+}
+
+type SignupPayload = pick<User, "username" | "name", "phone" | "password">;
+
+type LoginPayload = pick<User, "phone" | "password">;
+
+interface Post {
+  _id: string;
+  title: string;
+  content: string;
+  tags: string[];
+  mediaURLs: string[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+type CreatePostPayload = Pick<Post, "title" | "content">;
+
+type UpdatePostPayload = Pick<Post, "title" | "content">;
